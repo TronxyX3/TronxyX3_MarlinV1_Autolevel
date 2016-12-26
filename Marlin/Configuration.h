da11a4de7dddf1cfdbb2bb3c1e09aa887da41508
Budi Prakosa <iyok@deadmediafm.org>
2016-12-26 08:10:45 +0700

    tweak 3dtoday fix extruder temp loop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,7 +143,7 @@
 // 147 is Pt100 with 4k7 pullup
 // 110 is Pt100 with 1k pullup (non standard)
 
-#define TEMP_SENSOR_0 		501
+#define TEMP_SENSOR_0 		6
 #define TEMP_SENSOR_1 		0
 #define TEMP_SENSOR_2 		0
 #define TEMP_SENSOR_BED 	501
